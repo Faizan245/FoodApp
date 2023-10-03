abeaa849f5deccb28f230248a203dd68536ed4d7
Faizan Ansari <hussainfaizan1379@gmail.com>
2023-10-03 10:00:46 +0530

    front

--- mernapp/src/screens/Cart.js
+++ mernapp/src/screens/Cart.js
@@ -16,7 +16,7 @@ export default function Cart() {
   const handleCheckOut = async () => {
     let userEmail = localStorage.getItem("userEmail");
     // console.log(data,localStorage.getItem("userEmail"),new Date())
-    let response = await fetch("http://localhost:5000/api/auth/orderData", {
+    let response = await fetch("https://mealprep-back.onrender.com/api/auth/orderData", {
       
       method: 'POST',
       headers: {
--- mernapp/src/screens/MyOrder.js
+++ mernapp/src/screens/MyOrder.js
@@ -8,7 +8,7 @@ export default function MyOrder() {
 
     const fetchMyOrder = async () => {
         console.log(localStorage.getItem('userEmail'))
-        await fetch("http://localhost:5000/api/auth/myOrderData", {
+        await fetch("https://mealprep-back.onrender.com/api/auth/myOrderData", {
            
             method: 'POST',
             headers: {

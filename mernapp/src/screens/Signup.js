2ee2579762944ab6cbfabf598bd53090f988cba1
Faizan Ansari <hussainfaizan1379@gmail.com>
2023-10-26 11:21:45 +0530

    latest

--- mernapp/src/screens/Signup.js
+++ mernapp/src/screens/Signup.js
@@ -37,7 +37,7 @@ export default function Signup() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/auth/createuser", {
+    const response = await fetch("https://mealprep-back.onrender.com/api/auth/createuser", {
       
       method: 'POST',
       headers: {
